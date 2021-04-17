21e6b6356d47f17014d7f03877a3fe6c6f609b27
mimmiGitHub <80822491+mimmiGitHub@users.noreply.github.com>
2021-04-17 12:03:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+objectDetector="";
+inputName="";
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    inputName=document.getElementById("object_input");
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function draw(){
+    image(video,0,0,480,380);
+}
