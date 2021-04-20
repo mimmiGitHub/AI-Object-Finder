d6231d5aa8739a6d31c2238b347645e4d419acda
mimmiGitHub <80822491+mimmiGitHub@users.noreply.github.com>
2021-04-20 16:20:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 objectDetector="";
 inputName="";
+objects=[];
 function setup(){
     canvas=createCanvas(480,380);
     canvas.center();
@@ -17,4 +18,39 @@ function modelLoaded(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status !=""){
+    objectDetector.detect(video,gotResult);
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Detecting Objects";
+            document.getElementById("detected_objects").innerHTML="Number of objects detected are:"+objects.length;
+
+            fill('turquoise');
+            stroke('turquoise');
+            noFill();
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+        }
+        if(inputName == objects[i].label){
+            videoLiveView.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("status").innerHTML="Status: Object Mentioned Found";
+            synth=window.speechSynthesis;
+            data_to_speak="Object Mentioned Found";
+            utterThis=SpeechSynthesisUtterance(data_to_speak);
+            synth.speak(utterThis);
+        }
+        else{
+            document.getElementById("status").innerHTML="Status: Object Mentioned Not Found";
+        }
+    }
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+        objects=results;
+    }
 }
